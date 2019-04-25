751b28c9fe345dd4e4cd1cc3d5ef24a841b8d832
Lee Byungsun <buggyani@hanmail.net>
2019-04-25 13:09:53 +0900

    메일 레이아눗 변경

--- app/src/main/java/com/trenbe/test/MainActivity.kt
+++ app/src/main/java/com/trenbe/test/MainActivity.kt
@@ -6,22 +6,23 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import com.trenbe.test.ui.main.SectionsPagerAdapter
 import kotlinx.android.synthetic.main.activity_main.*
+import android.util.DisplayMetrics
+
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val metrics = resources.displayMetrics
+
         val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
+        println("density = $metrics")
         view_pager.adapter = sectionsPagerAdapter
         tabs.run {
             setupWithViewPager(view_pager)
             getTabAt(2)?.select()
-        }
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
         }
     }
 }
